4bb213011cd10555b7c77a4a735a8631cbffadea
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-04-02 14:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+let openModel = document.getElementById("openModal");
+openModel.addEventListener('click', function() {
+	let disPlayModel = document.getElementsByClassName("modal");
+	disPlayModel[0].style.display='block';
+});
+window.onclick = function(event) {
+  let modal = document.getElementsByClassName('modal')[0];
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+let closeModel = document.getElementsByClassName("close-modal-button");
+closeModel[0].addEventListener('click', function() {
+	let closeModal = document.getElementsByClassName("modal");
+	closeModal[0].style.display='none';
+});
+

a09932c0cb8e9aaef2b1a2893fdc546acf6a5fb7
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-04-02 15:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-//your JS code here. If required.
+// //your JS code here. If required.
 
-let openModel = document.getElementById("openModal");
-openModel.addEventListener('click', function() {
-	let disPlayModel = document.getElementsByClassName("modal");
-	disPlayModel[0].style.display='block';
-});
-window.onclick = function(event) {
-  let modal = document.getElementsByClassName('modal')[0];
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-let closeModel = document.getElementsByClassName("close-modal");
-closeModel[0].addEventListener('click', function() {
-	let closeModal = document.getElementsByClassName("modal");
-	closeModal[0].style.display='none';
-});
+// let openModel = document.getElementById("openModal");
+// openModel.addEventListener('click', function() {
+// 	let disPlayModel = document.getElementsByClassName("modal");
+// 	disPlayModel[0].style.display='block';
+// });
+// window.onclick = function(event) {
+//   let modal = document.getElementsByClassName('modal')[0];
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+// let closeModel = document.getElementsByClassName("close-modal");
+// closeModel[0].addEventListener('click', function() {
+// 	let closeModal = document.getElementsByClassName("modal");
+// 	closeModal[0].style.display='none';
+// });
+
+      var modal = document.querySelector(".modal");
+      var btn = document.querySelector("#openModal");
+      var span = document.querySelector(".close-modal");
+ 
+      btn.onclick = function () {
+        modal.style.display = "block";
+      };
+ 
+      span.onclick = function () {
+        modal.style.display = "none";
+      };
+ 
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      };
 
